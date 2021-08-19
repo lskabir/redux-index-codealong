c2ad735773e5bf272d7a2b6442abd244a5665968
Lutful Kabir <lutfulkbr@yahoo.com>
2021-08-19 11:46:20 -0400

    commit tree 1b98b0d4a9f47a674f110cdf07e7edf58fde3083

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,9 +1,6 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
+export default function manageTodo(state = {todos: []}, action) {
   switch (action.type) {
     case 'ADD_TODO':
-
       return { todos: state.todos.concat(action.payload.text) };
 
     default:
